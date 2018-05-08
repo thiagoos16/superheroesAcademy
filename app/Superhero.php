8e9050dcdee2b8ee5032982bc2adb676d8ed5412
thiagometal <thiagometal.fne@gmail.com>
2018-05-08 00:22:21 -0400

    add superhero model

---
+++ app/Superhero.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Superhero extends Model
+{
+    protected $table = 'superhero';
+
+    protected $fillable = ['nickname', 'real_name', 'origin_description', 'catch_phrase'];
+}
