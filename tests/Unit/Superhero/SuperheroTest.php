bd12c8b5f294ac75ec758da8675ec1c9cacd1e24
thiagometal <thiagometal.fne@gmail.com>
2018-05-11 15:59:14 -0400

    add find by id superhero test

--- tests/Unit/Superhero/SuperheroTest.php
+++ tests/Unit/Superhero/SuperheroTest.php
@@ -10,11 +10,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class SuperheroTest extends TestCase
 {
     use RefreshDatabase;
-
+    
     public function test_create_superhero()
     {
         $data = [
-            'nickname' => 'Spawn',
+            'nickname' => 'Superhero_Test_Case',
             'real_name' => 'AL Simmons',
             'origin_description' => 'Albert Francis "Al" Simmons (Lt. Colonel, USMC-Ret.), born in Detroit Michigan, was a highly trained Force Recon Marine who was at his most successful point when he saved the President from an attempted assassination...',
             'catch_phrase' => 'Nothing to Say'
@@ -23,5 +23,25 @@ class SuperheroTest extends TestCase
         $superhero = SuperheroController::storeSuperhero($data);
 
         $this->assertInstanceOf(Superhero::class, $superhero);
+
+        Superhero::find($superhero->id)->delete();
+    }
+
+    public function test_find_superhero_by_id()
+    {
+        $data = [
+            'nickname' => 'Superhero_Test_Case',
+            'real_name' => 'AL Simmons',
+            'origin_description' => 'Albert Francis "Al" Simmons (Lt. Colonel, USMC-Ret.), born in Detroit Michigan, was a highly trained Force Recon Marine who was at his most successful point when he saved the President from an attempted assassination...',
+            'catch_phrase' => 'Nothing to Say'
+        ];
+
+        $superhero = SuperheroController::storeSuperhero($data);
+
+        $superhero_find = SuperheroController::findSuperheroById($superhero->id);
+
+        $this->assertEquals($superhero->id, $superhero_find->id);
+
+        Superhero::find($superhero->id)->delete();
     }
 }
