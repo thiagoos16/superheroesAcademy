91585dbe931ba7c3fad4ff6469f3b66a068b013f
thiagometal <thiagometal.fne@gmail.com>
2018-05-11 18:15:55 -0400

    add attach image to superhero test

--- tests/Unit/Superhero/SuperheroTest.php
+++ tests/Unit/Superhero/SuperheroTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Superhero;
 use Tests\TestCase;
 use App\Http\Controllers\SuperheroController;
 use App\Superhero;
+use App\Images;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SuperheroTest extends TestCase
@@ -76,5 +77,31 @@ class SuperheroTest extends TestCase
         $this->assertTrue($superhero_edited);
 
         Superhero::find($superhero->id)->delete();
+    }
+
+    public function test_add_image_to_superhero() {
+        $data = [
+            'nickname' => 'Superhero_Test_Case',
+            'real_name' => 'AL Simmons',
+            'origin_description' => 'Albert Francis "Al" Simmons (Lt. Colonel, USMC-Ret.), born in Detroit Michigan, was a highly trained Force Recon Marine who was at his most successful point when he saved the President from an attempted assassination...',
+            'catch_phrase' => 'Nothing to Say'
+        ];
+
+        $superhero = SuperheroController::storeSuperhero($data);
+
+        $n_rand = rand(999999, 9999999);
+
+        $data_image = [
+            'path' => '../images/superhero-id_' . $superhero->id . '-test_image_' . $n_rand . '.jpg',
+            'superhero_id' => $superhero->id
+        ];
+
+        $image = SuperheroController::storeImage($data_image);
+
+        $this->assertInstanceOf(Images::class, $image);
+
+        Superhero::find($superhero->id)->delete();
+
+        Images::find($image->id)->delete();
     }
 }
